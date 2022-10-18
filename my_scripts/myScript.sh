05db88978ebcfa8a8e7a604342c2e3eff89d3007
Ahmed Alhendi <44529285+AAlhendi1707@users.noreply.github.com>
2022-10-18 08:05:43 +0100

    update on myscript

--- my_scripts/myScript.sh
+++ my_scripts/myScript.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#ENV CALC_CCF="YES" #1
-#ENV IMPUTE="NO" #2
-#ENV PATIENT_ID="Patient" #3
-#ENV N_ITER=1000 #4
-#ENV MIN_COVERAGE=30 #5
-#ENV BUILDTREE_CCF_THRESHOLD=0.1 #6
-#ENV BLOCKLIST_CLUSTER="None" #7
-
-#CMD /phylogicndt/myScript.sh ${CALC_CCF} ${IMPUTE} ${PATIENT_ID} ${N_ITER} ${MIN_COVERAGE} ${BUILDTREE_CCF_THRESHOLD} ${BLOCKLIST_CLUSTER}
+#MODULE="Cluster" #1
+#CALC_CCF="YES" #2
+#IMPUTE="NO" #3
+#PATIENT_ID="Patient" #4
+#N_ITER=1000 #5
+#MIN_COVERAGE=30 #6
+#BUILDTREE_CCF_THRESHOLD=0.1 #7
+#BLOCKLIST_CLUSTER="None" #8
+#CMD /phylogicndt/myScript.sh ${MODULE} ${CALC_CCF} ${IMPUTE} ${PATIENT_ID} ${N_ITER} ${MIN_COVERAGE} ${BUILDTREE_CCF_THRESHOLD} ${BLOCKLIST_CLUSTER}
 
 linesinsif=$(wc -l MySimulation_input.sif | awk '{print $1}')
 Ndrivergenes=$(wc -l Driver_genes_v1.0.txt | awk '{print $1}')
@@ -16,87 +16,127 @@ Ndrivergenes=$(wc -l Driver_genes_v1.0.txt | awk '{print $1}')
 Nsamples=$(expr $linesinsif - 1)
 
 echo "PhylogicNDT run parameters are:"
-echo "CALC_CCF $1"
-echo "IMPUTE $2"
-echo "PATIENT_ID $3"
-echo "N_ITER $4"
-echo "MIN_COVERAGE $5"
-echo "BUILDTREE_CCF_THRESHOLD $6"
-echo "BLOCKLIST_CLUSTER $7"
+echo "MODULE $1"
+echo "CALC_CCF $2"
+echo "IMPUTE $3"
+echo "PATIENT_ID $4"
+echo "N_ITER $5"
+echo "MIN_COVERAGE $6"
+echo "BUILDTREE_CCF_THRESHOLD $7"
+echo "BLOCKLIST_CLUSTER $8"
 echo "........................................"
 echo "Number of Time points in MySimulation_input.sif = $Nsamples"
 echo "Number of driver genes in Driver_genes_v1.0.txt = $Ndrivergenes"
 echo "........................................"
 
+if [ "$1" == "Cluster" ]; then
 
-echo "start clustering ..."
+    echo "start clustering ..."
+    ## if impuse and calc_ccf true do
 
-## if impuse and calc_ccf true do
-
-if [ "$1" == "YES" ] && [ "$2" == "YES" ]; then
-    echo "calc CALC_CCF and impute before clustering..."
-    python ../phylogicndt/PhylogicNDT.py Cluster -i $3 \
-    -sif MySimulation_input.sif \
-    -drv Driver_genes_v1.0.txt \
-    --n_iter $4 \
-    --min_coverage $5 \
-    --maf_input_type calc_ccf \
-    --impute \
-    --maf
-elif [ "$1" == "YES" ] && [ "$2" == "NO" ]; then
-    echo "calc_ccf before clustering..."
-    python ../phylogicndt/PhylogicNDT.py Cluster -i $3 \
-    -sif MySimulation_input.sif \
-    -drv Driver_genes_v1.0.txt \
-    --n_iter $4 \
-    --min_coverage $5 \
-    --maf_input_type calc_ccf \
-    --maf
-elif [ "$1" == "NO" ] && [ "$2" == "YES" ]; then
-    echo "impute before clustering..."
-    python ../phylogicndt/PhylogicNDT.py Cluster -i $3 \
-    -sif MySimulation_input.sif \
-    -drv Driver_genes_v1.0.txt \
-    --n_iter $4 \
-    --min_coverage $5 \
-    --impute \
-    --maf
-elif [ "$1" == "NO" ] && [ "$2" == "NO" ]; then
-    echo "No calc_ccf or impute applied before clustering..."
-    python ../phylogicndt/PhylogicNDT.py Cluster -i $3 \
-    -sif MySimulation_input.sif \
-    -drv Driver_genes_v1.0.txt \
-    --n_iter $4 \
-    --min_coverage $5 \
-    --maf
-else
+    if [ "$2" == "YES" ] && [ "$3" == "YES" ]; then
+        echo "calc CALC_CCF and impute before clustering..."
+        python ../phylogicndt/PhylogicNDT.py Cluster -i $4 \
+        -sif MySimulation_input.sif \
+        -drv Driver_genes_v1.0.txt \
+        --n_iter $5 \
+        --min_coverage $6 \
+        --maf_input_type calc_ccf \
+        --impute \
+        --maf
+    elif [ "$2" == "YES" ] && [ "$3" == "NO" ]; then
+        echo "calc_ccf before clustering..."
+        python ../phylogicndt/PhylogicNDT.py Cluster -i $4 \
+        -sif MySimulation_input.sif \
+        -drv Driver_genes_v1.0.txt \
+        --n_iter $5 \
+        --min_coverage $6 \
+        --maf_input_type calc_ccf \
+        --maf
+    elif [ "$2" == "NO" ] && [ "$3" == "YES" ]; then
+        echo "impute before clustering..."
+        python ../phylogicndt/PhylogicNDT.py Cluster -i $4 \
+        -sif MySimulation_input.sif \
+        -drv Driver_genes_v1.0.txt \
+        --n_iter $5 \
+        --min_coverage $6 \
+        --impute \
+        --maf
+    elif [ "$2" == "NO" ] && [ "$3" == "NO" ]; then
+        echo "No calc_ccf or impute applied before clustering..."
+        python ../phylogicndt/PhylogicNDT.py Cluster -i $4 \
+        -sif MySimulation_input.sif \
+        -drv Driver_genes_v1.0.txt \
+        --n_iter $5 \
+        --min_coverage $6 \
+        --maf
+    else
     echo "Could NOT phrase the passing input argment for clustering!"
-fi
+    fi
+    
+    echo "Clustering done ..."
+    echo "Adding aa change to mutation name ..."
 
+    ## adding protein change anno to mut.ccfs file
+    Rscript ../phylogicndt/merge.add.protein.anno.R ./ $4.mut_ccfs.txt
 
-## build tree
-echo "start building tree ..."
+    ## build tree
+    echo "start building tree ..."
 
-if [ "$7" == "None" ] || [ "$7" == "" ]; then
-    python ../phylogicndt/PhylogicNDT.py BuildTree -i $3 \
-    --seed 123 \
-    -sif MySimulation_input.sif \
-    -drv Driver_genes_v1.0.txt \
-    -m $3.mut_ccfs.txt \
-    -c $3.cluster_ccfs.txt \
-    --n_iter $4 \
-    --blacklist_threshold $6
+    if [ "$8" == "None" ] || [ "$8" == "" ]; then
+        python ../phylogicndt/PhylogicNDT.py BuildTree -i $4 \
+        --seed 123 \
+        -sif MySimulation_input.sif \
+        -drv Driver_genes_v1.0.txt \
+        -m $4.mut_ccfs.txt \
+        -c $4.cluster_ccfs.txt \
+        --n_iter $5 \
+        --blacklist_threshold $7
+    else
+	    python ../phylogicndt/PhylogicNDT.py BuildTree -i $4 \
+        --seed 123 \
+        -sif MySimulation_input.sif \
+        -drv Driver_genes_v1.0.txt \
+        -m $4.mut_ccfs.txt \
+        -c $4.cluster_ccfs.txt \
+        --n_iter $5 \
+        --blacklist_threshold $7 \
+        --blacklist_cluster $8
+    fi
+
+    echo "Finished!"
+
+elif [ "$1" == "BuildTree" ]; then
+
+    echo "Adding aa change to mutation name ..."
+    ## adding protein change anno to mut.ccfs file
+    Rscript ../phylogicndt/merge.add.protein.anno.R ./ $4.mut_ccfs.txt
+    ## build tree
+    echo "start building tree ..."
+
+    if [ "$8" == "None" ] || [ "$8" == "" ]; then
+        python ../phylogicndt/PhylogicNDT.py BuildTree -i $4 \
+        --seed 123 \
+        -sif MySimulation_input.sif \
+        -drv Driver_genes_v1.0.txt \
+        -m $4.mut_ccfs.txt \
+        -c $4.cluster_ccfs.txt \
+        --n_iter $5 \
+        --blacklist_threshold $7
+    else
+	    python ../phylogicndt/PhylogicNDT.py BuildTree -i $4 \
+        --seed 123 \
+        -sif MySimulation_input.sif \
+        -drv Driver_genes_v1.0.txt \
+        -m $4.mut_ccfs.txt \
+        -c $4.cluster_ccfs.txt \
+        --n_iter $5 \
+        --blacklist_threshold $7 \
+        --blacklist_cluster $8
+    fi
+
+    echo "Finished!"
+
 else
-	python ../phylogicndt/PhylogicNDT.py BuildTree -i $3 \
-    --seed 123 \
-    -sif MySimulation_input.sif \
-    -drv Driver_genes_v1.0.txt \
-    -m $3.mut_ccfs.txt \
-    -c $3.cluster_ccfs.txt \
-    --n_iter $4 \
-    --blacklist_threshold $6 \
-    --blacklist_cluster $7
+    echo "Inputted module is not recognised! The current supported module to run with this docker are: Cluster[default option] or BuildTree"
 fi
-
-echo "Finished!"
-
